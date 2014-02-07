a84f683fdf849a0a1cdcbbe05f03f8b825ed5c2c
Binit Kumar <binit.kumar.hbs@gmail.com>
2014-02-07 12:47:38 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sakuralive::Application.config.secret_key_base = '68a3fcd5d994f110834c72a27660225fb281b31e4b2e25f72e8ca731e8664e692a2d17c8851193e78ba47399d9ee8067c27eec3133ad6c70c27ceb78bc779d12'
